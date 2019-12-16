014e5c2110d7b1687ee19ee65723decd35c7b9a2
Chanseok Oh <chanseok@google.com>
2019-12-16 13:02:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/JLinkSample.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class JLinkSample {
+
+  public static void main(String[] args) throws Exception {
+    System.out.print("Hello world!");
+  }
+}
---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.example {
+  requires java.base;
+}
